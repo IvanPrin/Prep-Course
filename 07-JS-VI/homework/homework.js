9aee06e7dcb431625c4e12ffe7ebb34b45423edc
IvanPrin <ivanchancalay11@gmail.com>
2022-07-09 13:08:32 -0300

    Cambios 9/7

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -49,13 +49,11 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  const arraynuevo = [];
   const nuevos = array.map(function(elemento){
-    cb(elemento)
-  })
-  const nuevos1 = cb.map(function(elemento){
-    arraynuevo[elemento];
-  })
+    return cb(elemento);
+  });
+  return nuevos;
+  
 }
 
 function filter(array) {
